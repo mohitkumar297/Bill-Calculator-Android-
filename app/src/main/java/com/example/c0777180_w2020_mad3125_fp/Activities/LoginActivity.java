ffd41541b917d02eee19f810815284f930ff99e7
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 15:04:24 -0400

    [MK] IDs connected using Butterknife

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/LoginActivity.java
@@ -1,18 +1,27 @@
 package com.example.c0777180_w2020_mad3125_fp.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.c0777180_w2020_mad3125_fp.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @InjectView(R.id.txtLogin)
+    TextInputEditText txtLogin;
+    @InjectView(R.id.txtPassword)
+    TextInputEditText txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        ButterKnife.inject(this);
 
     }
 }

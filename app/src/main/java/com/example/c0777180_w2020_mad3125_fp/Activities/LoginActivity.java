2f4ee45fd9bcdec713bd143a0357fdcc268f595f
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 16:13:47 -0400

    [MK] Shared Preferences implemented

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.c0777180_w2020_mad3125_fp.Activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,16 +28,44 @@ public class LoginActivity extends AppCompatActivity {
     @InjectView(R.id.chkRemember)
     CheckBox chkRemember;
 
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    public static final String PREFS_NAME = "MyPrefsFile";
+    private static final String PREF_USERNAME = "username";
+    private static final String PREF_PASSWORD = "password";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.inject(this);
 
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        String email = sharedPreferences.getString(PREF_USERNAME, null);
+        String password = sharedPreferences.getString(PREF_PASSWORD, null);
+        txtLogin.setText(email);
+        txtPassword.setText(password);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (validateUser()) {
+
+                    if (chkRemember.isChecked()){
+                        editor.putString(PREF_USERNAME,txtLogin.getText().toString());
+                        editor.putString(PREF_PASSWORD,txtPassword.getText().toString());
+                        editor.apply();
+                    }
+
+                    else{
+                        editor.putString(PREF_USERNAME,"");
+                        editor.putString(PREF_PASSWORD,"");
+                        editor.apply();
+                    }
                     Intent intent = new Intent(LoginActivity.this, CustomerListActivity.class);
                     startActivity(intent);
                 } else {

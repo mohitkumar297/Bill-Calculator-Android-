663d19711d6d414e55edcfe0daf68aceaa32d103
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 15:21:02 -0400

    [MK] Added listener on button

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/LoginActivity.java
@@ -2,7 +2,8 @@ package com.example.c0777180_w2020_mad3125_fp.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,28 +20,34 @@ public class LoginActivity extends AppCompatActivity {
     TextInputEditText txtLogin;
     @InjectView(R.id.txtPassword)
     TextInputEditText txtPassword;
+    @InjectView(R.id.btnLogin)
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.inject(this);
-        
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validateUser()) {
+                    Intent intent = new Intent(LoginActivity.this, CustomerListActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Access Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
-        if (validateUser()){
-            Intent intent = new Intent(LoginActivity.this,CustomerListActivity.class);
-            startActivity(intent);
-        }
-        else {
-            Toast.makeText(LoginActivity.this,"Access Denied",Toast.LENGTH_SHORT).show();
-        }
     }
 
-    public boolean validateUser(){
-        if ((txtLogin.getText().toString() == "admin@gmail.com") && (txtPassword.getText().toString() == "admin123"))  {
+    public boolean validateUser() {
+        if ((txtLogin.getText().toString() == "admin@gmail.com") && (txtPassword.getText().toString() == "admin123")) {
             return true;
+        } else {
+            return false;
         }
-        else {return false;}
     }
 }

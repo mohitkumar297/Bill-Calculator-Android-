f321fbcf2443adf475dd43ed05056db482e34db7
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 15:19:49 -0400

    [MK] Added code for email and password validation

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.c0777180_w2020_mad3125_fp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +25,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.inject(this);
+        
 
+
+        if (validateUser()){
+            Intent intent = new Intent(LoginActivity.this,CustomerListActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(LoginActivity.this,"Access Denied",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean validateUser(){
+        if ((txtLogin.getText().toString() == "admin@gmail.com") && (txtPassword.getText().toString() == "admin123"))  {
+            return true;
+        }
+        else {return false;}
     }
 }

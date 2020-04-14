dcb1626c97522c60bd31f4a1bd35e1aa937db7eb
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 16:34:43 -0400

    [MK] Created a new class Hydro extending from Bill class

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Models/Bill.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Models/Bill.java
@@ -1,4 +1,18 @@
 package com.example.c0777180_w2020_mad3125_fp.Models;
 
-public class Bill {
+import java.time.LocalDate;
+import java.util.Date;
+
+enum BillType {
+    Mobile,
+    Hydro,
+    Internet
+}
+
+public abstract class Bill {
+        private String billID;
+        private LocalDate billDate;
+        private BillType billType;
+        private double totalBill;
+
 }

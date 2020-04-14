1511d352aa8d5dc9d44af29e14801fb186511c16
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 18:43:49 -0400

    [MK] DataRepo made singleton to acces data

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Util/DataRepo.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Util/DataRepo.java
@@ -1,5 +1,12 @@
 package com.example.c0777180_w2020_mad3125_fp.Util;
 
 public class DataRepo {
-    
+    private static DataRepo instance = new DataRepo();
+
+    private DataRepo() {
+    }
+    public static DataRepo getInstance(){
+        return instance;
+    }
+
 }

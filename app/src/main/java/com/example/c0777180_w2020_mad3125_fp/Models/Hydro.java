70b9983b8a47ad7a88b7df2efa64740819112d23
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 16:43:27 -0400

    [MK] Added getter and setter in hydro class

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Models/Hydro.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Models/Hydro.java
@@ -7,10 +7,25 @@ public class Hydro extends Bill {
     private String agencyName;
     private int unitsConsumed;
 
-    public Hydro(String billID, LocalDate billDate, BillType billType, double totalBill, ) {
+    public Hydro(String billID, LocalDate billDate, BillType billType, double totalBill, String agencyName, Integer unitsConsumed) {
         super(billID, billDate, billType, totalBill);
+        this.agencyName = agencyName;
+        this.unitsConsumed = unitsConsumed;
     }
 
+    public String getAgencyName() {
+        return agencyName;
+    }
 
-//    public Hydro(String billId, LocalDate billDate, BillType billType, Double totalBill, String agencyName,)
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    public int getUnitsConsumed() {
+        return unitsConsumed;
+    }
+
+    public void setUnitsConsumed(int unitsConsumed) {
+        this.unitsConsumed = unitsConsumed;
+    }
 }

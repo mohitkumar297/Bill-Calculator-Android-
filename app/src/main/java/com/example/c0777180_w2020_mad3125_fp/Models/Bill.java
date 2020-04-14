202392b320ec85dc747303c160189755baa26c56
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 16:42:19 -0400

    [MK] Created constructor in bill class

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Models/Bill.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Models/Bill.java
@@ -3,16 +3,24 @@ package com.example.c0777180_w2020_mad3125_fp.Models;
 import java.time.LocalDate;
 import java.util.Date;
 
-enum BillType {
-    Mobile,
-    Hydro,
-    Internet
-}
+
 
 public abstract class Bill {
+
+    enum BillType {
+        Mobile,
+        Hydro,
+        Internet
+    }
         private String billID;
         private LocalDate billDate;
         private BillType billType;
         private double totalBill;
 
+    public Bill(String billID, LocalDate billDate, BillType billType, double totalBill) {
+        this.billID = billID;
+        this.billDate = billDate;
+        this.billType = billType;
+        this.totalBill = totalBill;
+    }
 }
--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Models/Hydro.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Models/Hydro.java
@@ -1,5 +1,16 @@
 package com.example.c0777180_w2020_mad3125_fp.Models;
 
-public class Hydro {
-    
+import java.time.LocalDate;
+
+public class Hydro extends Bill {
+
+    private String agencyName;
+    private int unitsConsumed;
+
+    public Hydro(String billID, LocalDate billDate, BillType billType, double totalBill, ) {
+        super(billID, billDate, billType, totalBill);
+    }
+
+
+//    public Hydro(String billId, LocalDate billDate, BillType billType, Double totalBill, String agencyName,)
 }

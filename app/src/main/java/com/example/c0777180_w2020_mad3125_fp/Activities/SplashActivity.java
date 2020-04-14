75ab7cc7ef01539c58af9fdbc67b9be3323a536f
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-13 22:30:34 -0400

    [MK] Code for splash screen added

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/SplashActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/SplashActivity.java
@@ -2,15 +2,33 @@ package com.example.c0777180_w2020_mad3125_fp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.c0777180_w2020_mad3125_fp.R;
+
+import gr.net.maroulis.library.EasySplashScreen;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        EasySplashScreen config = new EasySplashScreen(SplashActivity.this)
+                .withFullScreen()
+                //.withTargetActivity((PersonInformationActivity.class))
+                .withSplashTimeOut(5000)
+                .withBackgroundColor(Color.parseColor("#1a1b29"))
+                .withAfterLogoText("Mohit Kumar\n\n  C0777180");
+                //.withLogo(R.drawable.ddollar);
+
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextSize(20);
+
+
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+
     }
 }

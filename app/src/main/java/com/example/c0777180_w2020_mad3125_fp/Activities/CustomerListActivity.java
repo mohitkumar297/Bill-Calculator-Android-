9a4d1d9b56a047ac7984bdb9030fba87f8b39aed
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 18:48:27 -0400

    [MK] Added Recycler View ID with butter knife

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/CustomerListActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/CustomerListActivity.java
@@ -1,17 +1,26 @@
 package com.example.c0777180_w2020_mad3125_fp.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.c0777180_w2020_mad3125_fp.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class CustomerListActivity extends AppCompatActivity {
+
+
+    @InjectView(R.id.recyclerView)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_list);
+        ButterKnife.inject(this);
 
     }
 }

2edda2f222700721c008ec9b490048051811bbb2
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 16:28:37 -0400

    [MK] Created a new class Bill

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Models/Customer.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Models/Customer.java
@@ -1,6 +1,12 @@
 package com.example.c0777180_w2020_mad3125_fp.Models;
 
+import java.util.List;
+
 public class Customer {
 
-    
+    private String customerID;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String emailID;
 }

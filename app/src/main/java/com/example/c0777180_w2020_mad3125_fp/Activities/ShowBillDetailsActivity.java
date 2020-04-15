734ce5c8ee2c010279d6f166232a2775cd0390b7
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 21:44:57 -0400

    [MK] Added an ID for text View

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/ShowBillDetailsActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/ShowBillDetailsActivity.java
@@ -1,18 +1,26 @@
 package com.example.c0777180_w2020_mad3125_fp.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.c0777180_w2020_mad3125_fp.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class ShowBillDetailsActivity extends AppCompatActivity {
+
+    @InjectView(R.id.textView)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_bill_details);
-
+        ButterKnife.inject(this);
+        
 
     }
 }

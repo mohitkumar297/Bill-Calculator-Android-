6ac119a885a0d5539d97caaebb7ae3afc74a6f46
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-14 21:48:09 -0400

    [MK] Data passed to next activity

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/ShowBillDetailsActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/ShowBillDetailsActivity.java
@@ -1,10 +1,12 @@
 package com.example.c0777180_w2020_mad3125_fp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.c0777180_w2020_mad3125_fp.Models.Customer;
 import com.example.c0777180_w2020_mad3125_fp.R;
 
 import butterknife.ButterKnife;
@@ -20,7 +22,9 @@ public class ShowBillDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_bill_details);
         ButterKnife.inject(this);
-        
 
+        Intent intent = getIntent();
+        Customer customer = intent.getParcelableExtra("CUSTOMERINFO");
+        textView.setText(customer.getFirstName().toUpperCase()+" "+customer.getEmailID().toUpperCase()+" "+ customer.getDateOfBirth());
     }
 }

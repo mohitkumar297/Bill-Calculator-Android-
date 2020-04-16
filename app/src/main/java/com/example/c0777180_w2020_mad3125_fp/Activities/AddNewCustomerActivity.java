b27f77e4bad8602ab155dbe1f37ee7c5f0cfde41
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-15 23:20:07 -0400

    [MK] Created ID for button and text fields

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/AddNewCustomerActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/AddNewCustomerActivity.java
@@ -1,17 +1,38 @@
 package com.example.c0777180_w2020_mad3125_fp.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.c0777180_w2020_mad3125_fp.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class AddNewCustomerActivity extends AppCompatActivity {
+
+    @InjectView(R.id.id)
+    EditText id;
+    @InjectView(R.id.fname)
+    EditText fname;
+    @InjectView(R.id.lname)
+    EditText lname;
+    @InjectView(R.id.gender)
+    EditText gender;
+    @InjectView(R.id.email)
+    EditText email;
+    @InjectView(R.id.dob)
+    EditText dob;
+    @InjectView(R.id.submit)
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_customer);
+        ButterKnife.inject(this);
 
     }
 }

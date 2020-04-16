1afcc60552efd3908733442ee6683cc3bf37ce88
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-15 21:34:44 -0400

    [MK] Two mobile bills created

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Util/DataRepo.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Util/DataRepo.java
@@ -3,6 +3,7 @@ package com.example.c0777180_w2020_mad3125_fp.Util;
 import com.example.c0777180_w2020_mad3125_fp.Models.Bill;
 import com.example.c0777180_w2020_mad3125_fp.Models.Customer;
 import com.example.c0777180_w2020_mad3125_fp.Models.Hydro;
+import com.example.c0777180_w2020_mad3125_fp.Models.Mobile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,6 +30,10 @@ public class DataRepo {
 
         Hydro H3 = new Hydro("HYD003","12/05/2020", Bill.BillType.Hydro,77.2,"Liquid flow",127);
 
+        Mobile M1 = new Mobile("MOB101","19/04/2020", Bill.BillType.Mobile,98,"6475406638",36,312,"Rogers","Mega Deal");
+
+        Mobile M2 = new Mobile("MOB102","10/04/2020", Bill.BillType.Mobile,54,"8054626109",41,452,"Bell","All Time ");
+
         Customer C1 = new Customer("C001","Steven","Gerrard","steven@gmail.com","10/05/1995","Male");
 
         Customer C2 = new Customer("C002","Sadio","Mane","sadiom@yahoo.com","10/05/1995","Male");
@@ -38,8 +43,11 @@ public class DataRepo {
         Customer C4 = new Customer("C004","James","Milner","jamilner@gmail.com","10/05/1995","Male");
 
         C1.addBilltoCustomer(H1);
+        C1.addBilltoCustomer(M1);
         C2.addBilltoCustomer(H2);
         C3.addBilltoCustomer(H3);
+        C4.addBilltoCustomer(M2);
+        
 //        C4.addBilltoCustomer("HYD001",H1);
 
         customerArrayList.add(C1);

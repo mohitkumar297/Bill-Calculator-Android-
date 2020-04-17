7d50d8c6c171b56b129c6d97d49eb24c9bb3aa81
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-17 15:42:42 -0400

    [MK] Fragments working fine

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/AddNewBillActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/AddNewBillActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.c0777180_w2020_mad3125_fp.BillViewAddPagerAdapter;
 import com.example.c0777180_w2020_mad3125_fp.HydroFragment;
@@ -19,17 +23,24 @@ public class AddNewBillActivity extends AppCompatActivity {
    // private AppBarLayout appBarLayout;
     private ViewPager viewPager;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_bill);
         tabLayout = findViewById(R.id.tabLayout);
         viewPager = findViewById(R.id.viewPager);
+
+
+
         BillViewAddPagerAdapter adapter = new BillViewAddPagerAdapter(getSupportFragmentManager(),0);
         adapter.AddFragment(new MobileFragment(),"MOBILE ");
         adapter.AddFragment(new HydroFragment(),"HYDRO ");
         adapter.AddFragment(new InternetFragment(),"INTERNET ");
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+
+
     }
 }
--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/MobileFragment.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/MobileFragment.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MobileFragment extends Fragment {
+
+    private TextView kumar;
+    private EditText mohit;
+    private Button btnme;
 
     View view;
     public MobileFragment() {
@@ -18,7 +25,20 @@ public class MobileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         view = inflater.inflate(R.layout.mobile_fragment,container,false);
+
+        kumar = view.findViewById(R.id.kumar);
+        mohit = view.findViewById(R.id.mohit);
+        btnme = view.findViewById(R.id.btnme);
+
+        btnme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kumar.setText(mohit.getText().toString());
+            }
+        });
+
         return view;
     }
 }

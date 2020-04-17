be21bf8bc653ec6c2b4ea98facea30090d98c00d
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-17 16:02:38 -0400

    [MK] Created new Hydro project

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/HydroFragment.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/HydroFragment.java
@@ -4,15 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.c0777180_w2020_mad3125_fp.Models.Bill;
+import com.example.c0777180_w2020_mad3125_fp.Models.Hydro;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class HydroFragment extends Fragment {
 
     View view;
-    
+    @InjectView(R.id.hydroID)
+    EditText hydroID;
+    @InjectView(R.id.hydroBillDate)
+    EditText hydroBillDate;
+    @InjectView(R.id.hydroBillAmount)
+    EditText hydroBillAmount;
+    @InjectView(R.id.hydroAgencyName)
+    EditText hydroAgencyName;
+    @InjectView(R.id.hydroUnitsConsumed)
+    EditText hydroUnitsConsumed;
+
 
     public HydroFragment() {
     }
@@ -20,7 +37,12 @@ public class HydroFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.hydro_fragment,container,false);
+        view = inflater.inflate(R.layout.hydro_fragment, container, false);
+        ButterKnife.inject(this, view);
+
+        Double bill = Double.parseDouble(hydroBillAmount.getText().toString());
+        Integer units = Integer.parseInt(hydroUnitsConsumed.getText().toString());
+        Hydro hydro = new Hydro(hydroID.getText().toString(),hydroBillDate.getText().toString(), Bill.BillType.Hydro,bill,hydroAgencyName.getText().toString(),units);
         return view;
     }
 }

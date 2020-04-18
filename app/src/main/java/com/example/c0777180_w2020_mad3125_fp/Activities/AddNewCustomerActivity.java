66c01957376b623ca4a8d3bc9eb9e44bcc333f27
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-18 13:36:55 -0400

    [MK] Date Picker Added

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/AddNewCustomerActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/AddNewCustomerActivity.java
@@ -1,11 +1,12 @@
 package com.example.c0777180_w2020_mad3125_fp.Activities;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -14,23 +15,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.c0777180_w2020_mad3125_fp.Models.Customer;
 import com.example.c0777180_w2020_mad3125_fp.R;
 import com.example.c0777180_w2020_mad3125_fp.Util.DataRepo;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class AddNewCustomerActivity extends AppCompatActivity {
 
-    @InjectView(R.id.id)
-    EditText id;
-    @InjectView(R.id.fname)
-    EditText fname;
-    @InjectView(R.id.lname)
-    EditText lname;
 
-    @InjectView(R.id.email)
-    EditText email;
-    @InjectView(R.id.dob)
-    EditText dob;
     @InjectView(R.id.submit)
     Button submit;
     @InjectView(R.id.radioMale)
@@ -41,7 +37,25 @@ public class AddNewCustomerActivity extends AppCompatActivity {
     RadioButton radioOther;
     @InjectView(R.id.radio)
     RadioGroup radio;
+
     String checkedBox = "";
+
+    DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
+    @InjectView(R.id.id)
+    TextInputEditText id;
+    @InjectView(R.id.fname)
+    TextInputEditText fname;
+    @InjectView(R.id.lname)
+    TextInputEditText lname;
+    @InjectView(R.id.email)
+    TextInputEditText email;
+    @InjectView(R.id.dob)
+    TextInputEditText dob;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +68,22 @@ public class AddNewCustomerActivity extends AppCompatActivity {
                 int checkedRadio = radio.getCheckedRadioButtonId();
                 RadioButton checkedRadioButton = findViewById(checkedRadio);
                 checkedBox = checkedRadioButton.getText().toString();
+            }
+        });
+
+
+
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(AddNewCustomerActivity.this,R.style.DialogTheme, datePickerListener, year, month, dayOfMonth);
+                datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
+                datePickerDialog.show();
             }
         });
 
@@ -71,6 +101,21 @@ public class AddNewCustomerActivity extends AppCompatActivity {
             }
         });
     }
+
+    DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            Calendar c = Calendar.getInstance();
+            c.set(Calendar.YEAR, year);
+            c.set(Calendar.MONTH, month);
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            String format = new SimpleDateFormat("dd-MM-YYYY").format(c.getTime());
+            dob.setText(format);
+            //FOR AGE DISPLAY txtTaxFilingDate.setText(Integer.toString(calculateAge(c.getTimeInMillis())));
+        }
+
+
+    };
 
     public void maleClick(View view) {
         radioMale.setTextColor(Color.WHITE);

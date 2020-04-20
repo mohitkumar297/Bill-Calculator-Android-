084a89e8f006cb6b61184a784c66adca23b33dfa
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-19 20:08:10 -0400

    [MK] Added drawables

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/MobileFragment.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/MobileFragment.java
@@ -14,10 +14,6 @@ import androidx.fragment.app.Fragment;
 
 public class MobileFragment extends Fragment {
 
-    private TextView kumar;
-    private EditText mohit;
-    private Button btnme;
-
     View view;
     public MobileFragment() {
     }
@@ -28,16 +24,6 @@ public class MobileFragment extends Fragment {
 
         view = inflater.inflate(R.layout.mobile_fragment,container,false);
 
-        kumar = view.findViewById(R.id.kumar);
-        mohit = view.findViewById(R.id.mohit);
-        btnme = view.findViewById(R.id.btnme);
-
-        btnme.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                kumar.setText(mohit.getText().toString());
-            }
-        });
 
         return view;
     }

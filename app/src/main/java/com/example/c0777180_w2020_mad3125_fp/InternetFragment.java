85777f1c43bec03081fb53ca865e0e6b65cad9ec
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-19 20:53:37 -0400

    [MK] Added IDs in internet bill form

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/InternetFragment.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/InternetFragment.java
@@ -5,34 +5,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 import java.text.SimpleDateFormat;
-import java.time.OffsetDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class InternetFragment extends Fragment {
-   View view;
+    View view;
     DatePickerDialog datePickerDialog;
     int year;
     int month;
     int dayOfMonth;
     Calendar calendar;
+    @InjectView(R.id.internetID)
+    TextInputEditText internetID;
+    @InjectView(R.id.internetBillDate)
+    TextInputEditText internetBillDate;
+    @InjectView(R.id.internetAgencyName)
+    TextInputEditText internetAgencyName;
+    @InjectView(R.id.internetUnitsConsumed)
+    TextInputEditText internetUnitsConsumed;
+    @InjectView(R.id.internetBillAmount)
+    TextInputEditText internetBillAmount;
+    @InjectView(R.id.internetSave)
+    Button internetSave;
+
     public InternetFragment() {
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.internet_fragment,container, false);
+        view = inflater.inflate(R.layout.internet_fragment, container, false);
+        ButterKnife.inject(this, view);
 
 
-        hydroBillDate.setOnClickListener(new View.OnClickListener() {
+        internetBillDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -40,7 +59,7 @@ public class InternetFragment extends Fragment {
                 year = calendar.get(Calendar.YEAR);
                 month = calendar.get(Calendar.MONTH);
                 dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-                datePickerDialog = new DatePickerDialog(getActivity(),R.style.DialogTheme, datePickerListener, year, month, dayOfMonth);
+                datePickerDialog = new DatePickerDialog(getActivity(), R.style.DialogTheme, datePickerListener, year, month, dayOfMonth);
                 datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
                 datePickerDialog.show();
             }
@@ -56,7 +75,7 @@ public class InternetFragment extends Fragment {
             c.set(Calendar.MONTH, month);
             c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
             String format = new SimpleDateFormat("dd-MM-YYYY").format(c.getTime());
-            dob.setText(format);
+            internetBillDate.setText(format);
             //FOR AGE DISPLAY txtTaxFilingDate.setText(Integer.toString(calculateAge(c.getTimeInMillis())));
         }
 

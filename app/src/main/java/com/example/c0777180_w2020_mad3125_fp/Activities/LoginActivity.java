ed9f1aecc6abbb37e25834965516ebd29a0fbb39
mohitkumar297 <60120967+mohitkumar297@users.noreply.github.com>
2020-04-20 11:56:15 -0400

    [MK] Alert Added in Login Activity

--- app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/c0777180_w2020_mad3125_fp/Activities/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.c0777180_w2020_mad3125_fp.Activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +11,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.c0777180_w2020_mad3125_fp.R;
@@ -28,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     @InjectView(R.id.chkRemember)
     CheckBox chkRemember;
 
-
+    AlertDialog.Builder builder;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
 
@@ -41,6 +44,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.inject(this);
+        builder = new AlertDialog.Builder(this);
 
         sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         editor = sharedPreferences.edit();
@@ -70,7 +74,21 @@ public class LoginActivity extends AppCompatActivity {
                     startActivity(intent);
                     finish();
                 } else {
-                    Toast.makeText(LoginActivity.this, "Invalid User Id or Password", Toast.LENGTH_SHORT).show();
+                    builder.setMessage("Invalid Username or Password")
+                            .setCancelable(false)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    Intent mIntent = new Intent(LoginActivity.this,LoginActivity.class);
+                                    startActivity(mIntent);
+                                    finish();
+                                }
+                            });
+                    //Creating dialog box
+                    AlertDialog alert = builder.create();
+                    //Setting the title manually
+                    alert.setTitle("Access Denied!");
+                    alert.show();
+                    alert.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(Color.BLACK);
                 }
             }
         });
